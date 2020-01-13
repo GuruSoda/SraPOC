ed95364f6a0755e2002af026ffe1cd7b80aedd7d
Fabricio <fabricio.rossi@gmail.com>
2020-01-13 01:29:46 -0300

    * Hola materializecss, chau bootstrap. * Se saco la logica de mame de la rutas y se puso en directorio llamado models. * Se creo una clase que representa el modelo de datos del mame. * Se cambio de lugar el archivo mame y se puso en un directorio llamo databases.

---
+++ models/armame.js
@@ -0,0 +1,178 @@
+const config = require('../configs/armame')
+
+const sqlite3 = require('sqlite3').verbose();
+
+class arMame {
+    constructor () {
+        this._DBFile = ""
+    }
+
+    Open(DBFile) {
+
+        if (DBFile)
+            this.db = new sqlite3.Database(DBFile, sqlite3.OPEN_READONLY)
+        else
+            this.db = new sqlite3.Database(config.database, sqlite3.OPEN_READONLY)
+    
+        return this
+    }
+
+    Close() {
+        this.db.close();
+    }
+
+
+    // Primera version donde carga todos los nombres a memoria
+    GamesMemory() {
+        const that = this
+
+        return new Promise(function(resolve, reject) {
+            let vector = []
+
+            that.db.all("SELECT name from game", vector, (error, rows) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    for (let i=0;i<rows.length;i++) {
+                        vector.push(rows[i].name)
+                    }
+                }
+                resolve(vector)
+            })
+        })
+    }
+
+    Games() {
+
+        const that = this
+
+        return new Promise(function (resolve, reject) {
+            let nombres = new Array()
+
+            that.db.parallelize(function() {
+                that.db.each("SELECT name from game order by name asc", function(err, row) {
+                    if (err) reject(err)
+                    else nombres.push(row.name)
+                }, function(error, num_rows) {
+                    if (error) reject(err)
+                    else resolve(nombres)
+                });
+            })
+         })
+    }
+
+    GamesDescription() {
+        const that = this
+
+        return new Promise(function (resolve, reject) {
+            let vector = []
+
+            that.db.parallelize(function() {
+                that.db.each("SELECT name, description from game", function(err, row) {
+                    vector.push({name: row.name, description: row.description})
+               }, function(error, total) {
+                   if (error) reject(error)
+                   else resolve(vector)
+               })
+            })
+        })
+    }
+
+    Game(name) {
+
+        const that = this
+
+        return new Promise(function(resolve, reject) {
+            let juego = {}
+
+            that.db.parallelize(function() {
+                that.db.get(`
+                    select g.id_game, g.name, g.description, m.name as 'manufacturer' , s.name as 'sourcefile', romof, cloneof, isbios, isdevice, ismechanical, year, runnable 
+                    from game g, sourcefile s, manufacturer m 
+                    where m.id_manufacturer = g.manufacturer and s.id_sourcefile = g.sourcefile and g.name = '${name}'`, 
+                    function(err, row) {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            if (!row) {
+                                resolve({})
+                            } else {
+                                juego = row
+                                
+                                that.db.all(`
+                                    select r.name, r.size, r.crc, r.sha1, t.text  
+                                    from game g, rom r, game_rom gr, text t 
+                                    where r.id_rom = gr.id_rom and g.id_game = gr.id_game and r.status = t.id_text and g.id_game = ${juego.id_game}`, 
+                                    function(err, rows) {
+                                        if (err) {
+                                            juego.roms = []
+                                        } else {
+                                            juego.roms = rows
+                                
+                                            resolve(juego)
+                                        }
+                                    })
+                            }
+                        }
+                })
+            })
+        })
+    }
+
+/*
+// estaba en las rutas
+// arreglar para que la llamada sea en cascada
+router.get('/gamesv2/:name', function(req, res, next) {
+
+    let juego = {}
+    let id_juego = 0
+
+    db.serialize(function() {
+        db.get(`
+            select g.id_game, g.name, g.description, m.name as 'manufacturer' , s.name as 'sourcefile', romof, cloneof, isbios, isdevice, ismechanical, year, runnable 
+            from game g, sourcefile s, manufacturer m 
+            where g.name = '${req.params.name}' and m.id_manufacturer = g.manufacturer and s.id_sourcefile = g.sourcefile`, 
+            function(err, row) {
+                if (err) {
+                    res.json('{}')
+                    next()
+                    return 
+                } else {
+                    if (!row) {
+                        res.json('{}')
+                        next()
+                        return  
+                    } else {
+                    juego = row
+                    console.log('juego:', juego)
+                    id_juego = row.id_game
+                    console.log(`id_game: ${id_juego}`)
+                    }
+                }
+        }).all(`
+            select r.name, r.size, r.crc, r.sha1, t.text  
+            from game g, rom r, game_rom gr, text t 
+            where r.id_rom = gr.id_rom and g.id_game = gr.id_game and r.status = t.id_text and g.name = '${req.params.name}'`, 
+            function(err, rows) {
+                console.log('all: id_game = ', id_juego)
+                console.log('all: err = ', err)
+                console.log('all: rows = ', rows)
+                if (err) {
+                    juego.roms = err
+                    next()
+                    return
+                } else
+                    juego.roms = rows
+            
+            res.json(juego)
+        });
+    })
+});
+*/
+
+    static instance(database) {
+        return new arMame(database)
+    }
+}
+
+module.exports = arMame
--- routes/mame.js
+++ routes/mame.js
@@ -1,142 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('mame.sqlite', sqlite3.OPEN_READONLY);
+const mame = require('../models/armame')
 
-// db.close();
+const armame = new mame()
+
+armame.Open()
 
 router.get('/gamesv3', function(req, res) {
     
-        let vector = []
+    armame.GamesMemory().then(function(nombres) {
+        res.json(nombres)
+    }).catch(function(error) {
+        res.json([])
+        console.log(error)
+    })
 
-        db.all("SELECT name from game", vector, (error, rows) => {
-            if (error) {
-                res.json(["error"])
-            } else {
-                for (let i=0;i<rows.length;i++) {
-                    vector.push(rows[i].name)
-                }
-            }
-
-            res.json(vector)
-        })
 });
 
 router.get('/gamesv2', function(req, res) {
-    
-    let vector = []
 
-    db.serialize(function() {
-        db.each("SELECT name, description from game", function(err, row) {
-            vector.push({name: row.name, description: row.description})
-       }, function(error, total) {
-            res.json(vector)
-       });
-    });
+    armame.GamesDescription().then(function(nombres) {
+        res.json(nombres)
+    }).catch(function(error) {
+        res.json([])
+        console.log(error)
+    })
+
 });
 
 router.get('/games', function(req, res) {
-    
-    let vector = []
 
-    db.serialize(function() {
-        db.each("SELECT name from game order by name asc", function(err, row) {
-            if (err) res.json([])
-            else vector.push(row.name)
-        }, function(error, num_rows) {
-            if (error) res.json([])
-            else res.json(vector)
-        });
+    armame.Games().then(function(nombres) {
+        res.json(nombres)
+    }).catch(function(error) {
+        res.json([])
+        console.log(error)
     })
-});
 
-// arreglar para que la llamada sea en cascada
-router.get('/gamesv2/:name', function(req, res, next) {
-
-    let juego = {}
-    let id_juego = 0
-
-    db.serialize(function() {
-        db.get(`
-            select g.id_game, g.name, g.description, m.name as 'manufacturer' , s.name as 'sourcefile', romof, cloneof, isbios, isdevice, ismechanical, year, runnable 
-            from game g, sourcefile s, manufacturer m 
-            where g.name = '${req.params.name}' and m.id_manufacturer = g.manufacturer and s.id_sourcefile = g.sourcefile`, 
-            function(err, row) {
-                if (err) {
-                    res.json('{}')
-                    next()
-                    return 
-                } else {
-                    if (!row) {
-                        res.json('{}')
-                        next()
-                        return  
-                    } else {
-                    juego = row
-                    console.log('juego:', juego)
-                    id_juego = row.id_game
-                    console.log(`id_game: ${id_juego}`)
-                    }
-                }
-        }).all(`
-            select r.name, r.size, r.crc, r.sha1, t.text  
-            from game g, rom r, game_rom gr, text t 
-            where r.id_rom = gr.id_rom and g.id_game = gr.id_game and r.status = t.id_text and g.name = '${req.params.name}'`, 
-            function(err, rows) {
-                console.log('all: id_game = ', id_juego)
-                console.log('all: err = ', err)
-                console.log('all: rows = ', rows)
-                if (err) {
-                    juego.roms = err
-                    next()
-                    return
-                } else
-                    juego.roms = rows
-            
-            res.json(juego)
-        });
-    })
 });
 
 router.get('/games/:name', function(req, res) {
 
-    let juego = {}
-
-    db.serialize(function() {
-        db.get(`
-            select g.id_game, g.name, g.description, m.name as 'manufacturer' , s.name as 'sourcefile', romof, cloneof, isbios, isdevice, ismechanical, year, runnable 
-            from game g, sourcefile s, manufacturer m 
-            where m.id_manufacturer = g.manufacturer and s.id_sourcefile = g.sourcefile and g.name = '${req.params.name}'`, 
-            function(err, row) {
-                if (err) {
-                    res.json({})
-                } else {
-                    if (!row) {
-                        res.json({})
-                    } else {
-                        juego = row
-                        
-                        db.all(`
-                            select r.name, r.size, r.crc, r.sha1, t.text  
-                            from game g, rom r, game_rom gr, text t 
-                            where r.id_rom = gr.id_rom and g.id_game = gr.id_game and r.status = t.id_text and g.id_game = ${juego.id_game}`, 
-                            function(err, rows) {
-                                if (err) {
-                                    juego.roms = []
-                                } else {
-                                    juego.roms = rows
-
-//                                    console.log(juego)
-                                    console.log(JSON.stringify(juego))
-                        
-                                    res.json(juego)
-                                }
-                            })
-                    }
-                }
-        })
+    armame.Game(req.params.name).then(function(juego) {
+        console.log(JSON.stringify(juego))
+        res.json(juego)
+    }).catch(function(error) {
+        res.json({})
+        console.log(error)
     })
+
 });
 
 module.exports = router;
