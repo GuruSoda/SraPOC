5d852ee9dbb22f49b4789e44fe0280f534467dca
Rossi <fabricio.rossi@gmail.com>
2020-01-27 19:49:55 -0300

    Se agrego callback cuando termine de cargar el partial

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,10 +1,12 @@
-function load(selector, url) {
+function load(selector, url, cb) {
     var request = new XMLHttpRequest()
 
     // se ejecuta cuando finalizo el GET
     request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
             document.getElementById(selector).innerHTML = request.responseText
+
+            if (cb) cb()
         }
     }
 
